203261ae4d1bff3463063bfa2c8decef87902587
ganeshpaiya <36655930+ganeshpaiya@users.noreply.github.com>
2018-06-16 01:05:43 +0530

    Add files via upload

---
+++ Scripts/myApp.js
@@ -0,0 +1,15 @@
+﻿var myApp = angular.module('myApp', []);
+myApp.controller('empctrl', function ($scope, $http)
+{
+    $scope.emps = ""
+    $http.get("/Index/Getemp")
+    .then(function (data) {
+        alert(data.data);
+        $scope.emps = data.data;
+      
+    }, function (result) {
+        //some error
+        console.log(result);
+    });
+
+});
